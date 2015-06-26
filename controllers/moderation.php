d2042b6ec3b8c396f2b32e05932218f1ce9ea7f4
arnaudc <a.clarebots@gmail.com>
2015-06-26 11:18:01 +0200

    building admin(15) built espace moderation built edit on espace administration

--- controllers/moderation.php
+++ controllers/moderation.php
@@ -8,4 +8,97 @@ if(!isset($_SESSION['user']) || $_SESSION['sid'] != session_id() || $_SESSION['u
 
 $liste_categories = getListCategory();
 
-$liste_photos = getListPhoto();
+// treat edit and delete
+
+if(isset($_GET['id']) && ctype_digit($_GET['id'])){
+
+    $action_photo = $_GET['id'];
+
+    $photo = getPhoto($action_photo);
+
+    // action
+
+    if(isset($_GET['action'])){
+
+        $action = secure($_GET['action']);
+
+        $autorized_actions = array('edit','treatedit');
+
+        if(in_array($action, $autorized_actions)){
+
+            // do nothing for edit and delete
+
+            // but treat treatedit and treatdelete
+
+            if($action == 'treatedit'){
+
+                if(isset($_POST['letitre']) && isset($_POST['ladesc'])){
+
+                    $letitre = secure($_POST['letitre']);
+                    $ladesc  = secure($_POST['ladesc']);
+
+                    unbindPhotoCategory($photo['id']);
+
+                    if(isset($_POST['category']) && is_array($_POST['category'])){
+
+                        $category = $_POST['category'];
+
+                    }else{
+
+                        $category = [];
+
+                    }
+
+                    foreach ($category as $value){
+
+                        bindPhotoCategory($photo['id'],$value);
+
+                    }
+
+                    $update_photo = updatePhoto($photo['id'], $letitre, $ladesc);
+
+                    if($update_photo == false){
+
+                        $editerror = 'La modification a échoué !';
+
+                    }
+
+                    // errors ? stack in a variable and show them
+
+                }else{
+
+                    header('Location: ./?page=deconnect');
+
+                }
+
+            }
+            
+        }else{
+
+            header('Location: ./?page=deconnect');
+
+        }
+
+    }else{
+
+        header('Location: ./?page=deconnect');
+
+    }
+
+}
+
+// pagination
+    
+if(isset($_GET['pos'])){ 
+
+    $pos = secure($_GET['pos']);
+
+}else{
+
+    $pos = 1;
+
+}
+
+$from = ($pos -1)*20;
+
+$liste_photos = getListPhoto($from);
